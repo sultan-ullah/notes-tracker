73911a7fd9b95156bbb153fdea1f72642cecdc6c
Sultan Ullah <sultan.ullah7@gmail.com>
2019-09-01 14:26:09 -0700

    minimal viable product

---
+++ Note.js
@@ -0,0 +1,163 @@
+/**
+ * Notes class to keep track of each individual note created
+ */
+const fs = require('fs')
+const filePath = './notes.json'
+
+/**
+ * @class
+ */
+class Note {
+
+    /**
+     * Represents a note with the required meta data
+     * @constructor
+     * @param {string} title - the title of the note 
+     * @param {string} text - the main body text of the note
+     * @param {string} timestamp - timestamp of the created date 
+     * @param {string} path - file path of the ntoe 
+     * @param {integer} id - id of the note created
+     */
+    constructor(title, text, timestamp = null, path = null, id = null) {
+        const date = new Date();
+        this.title = title;
+        this.text = text;
+        this.timestamp = timestamp ? timestamp : date.getTime();
+        this.path = path ? path : `./notes/${this.title.replace(" ", "_")}_${this.timestamp}.txt`;
+        this.id = id ? id : this.getID();
+    }
+
+    /**
+     * Add a new note
+     * Returns a promise for adding to notes.json file and creating note .txt file
+     */
+    add() {
+        return new Promise((resolve, reject) => {
+            Note.readJSONData()
+            .then(json => {
+                json.list.push({...this})
+                Note.writeJSONData(json)
+                fs.writeFile(this.path, this.text, (err) => {
+                    if (err) reject(err)
+                    resolve()
+                }); 
+            })
+            .catch((err) => reject(err))
+        });
+        
+    }
+
+    /**
+     * Get ID of the note from the notes.json file
+     */
+    getID() {
+        let data = fs.readFileSync(filePath);
+        let json = JSON.parse(data.toString());
+        return json.list.length;
+    }
+
+    /**
+     * Update a note
+     * Returns a promise for updating the notes.json file and the note .txt file
+     * @param {string} title 
+     * @param {string} text 
+     */
+    update(title, text) {
+        return new Promise((resolve, reject) => {
+            Note.readJSONData()
+            .then(json => {
+                this.title = title;
+                this.text = text;
+                json.list[this.id] = {...this}
+                Note.writeJSONData(json)
+                fs.writeFile(this.path, text, (err) => {
+                    if (err) reject(err)
+                    resolve()
+                });
+            })
+            .catch((err) => reject(err));
+        });
+    }
+
+    /**
+     * Delete a note
+     * Returns a promise for removing the note from the json file and deleting the .txt file
+     */
+    delete() {
+        return new Promise((resolve, reject) => {
+            Note.readJSONData()
+            .then(json => {
+                json.list.splice(this.id, 1)
+                let updatedIDs = json.list.map((item, index) => {
+                    return {...item, id : index}
+                });
+                json.list = updatedIDs;
+                Note.writeJSONData(json)
+                fs.unlink(this.path, (err) => {
+                    if (err) reject(err);
+                    resolve();
+                })
+            })
+            .catch((err) => reject(err));
+        });
+    }
+
+    static getNote(id) {
+        return new Promise((resolve, reject) => {
+            Note.readJSONData()
+            .then(json => {
+                let item = json.list[id];
+                if (item) {
+                    let { title, text, timestamp, path} = item;
+                    let note = new Note(title, text, timestamp, path, id);
+                    resolve(note)    
+                } else {
+                    reject("Item could not be found")
+                }
+            });
+        });
+    }
+
+    /**
+     * Retrieves a list of all the notes through the json file
+     * Returns a promise
+     */
+    static getNotesList() {
+        return new Promise((resolve, reject) => {
+            Note.readJSONData()
+                .then((json) => {
+                    resolve(json.list);
+                })
+                .catch(err => reject(err));
+        });
+    }
+
+    /**
+     * Reads JSON data from notes.json file (helper method)
+     * Returns a promise when reading from the file is done
+     */
+    static readJSONData() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, (err, data) => {
+                if (err) reject(err)
+                resolve(JSON.parse(data))
+            });
+        });
+    }
+
+    /**
+     * Writes to the notes.json file (helper method)
+     * Returns a promise when writing to the file is done
+     * @param {json} data 
+     */
+    static writeJSONData(data) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(filePath, JSON.stringify(data, null, 2), err => {
+                if (err) reject(err)
+                resolve()
+            });
+        });
+    }
+}
+
+module.exports.Note = Note;
---
+++ app.js
@@ -0,0 +1,129 @@
+const fs = require('fs')
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 5000
+const { Note } = require('./Note');
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+app.use(express.static('public/'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    createDirForNotes()
+        .catch(err => console.log(err))
+});
+
+// INDEX
+app.get('/', (req, res) => {
+    Note.getNotesList()
+        .then((list) => {
+            res.render('index', { "title" : "Note Keeper", "list" : formatList(list).reverse() });
+        })
+        .catch(err => console.log(err));
+});
+
+// CREATE
+app.get('/create', (req, res) => res.render('create'));
+app.post('/create', (req, res) => {
+    let newNote = new Note(req.body.title, req.body.text);
+    newNote.add()
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
+});
+
+// SHOW
+app.get('/notes/:id', (req, res) => {
+    Note.getNotesList()
+        .then((list) => {
+            let selected = list[req.params.id];
+            res.render('show', {
+                id : selected.id,
+                title : selected.title, 
+                text : selected.text,
+                created: formatDate(selected.timestamp)
+            });
+        });
+});
+
+// EDIT
+app.get('/notes/:id/edit', (req, res) => {
+    Note.getNotesList()
+        .then((list) => {
+            let selected = list[req.params.id];
+            res.render('edit', {
+                id : selected.id,
+                title : selected.title, 
+                text : selected.text,
+                created: formatDate(selected.timestamp)
+            });
+        });
+});
+
+// UPDATE
+app.put('/notes/:id/edit', (req, res) => {
+    Note.getNote(req.params.id)
+        .then((note) => {
+            note.update(req.body.title, req.body.text)
+        })
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err))
+});
+
+// DELETE
+app.delete('/notes/:id/delete', (req, res) => {
+    Note.getNote(req.params.id)
+        .then((note) => {
+            note.delete()
+        })
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err));
+});
+
+// HELPER FUNCTIONS
+
+/** Create a directory to keep the notes */
+const createDirForNotes = () => {
+    return new Promise((resolve, reject) => {
+        fs.stat('./notes/', (err, stats) => {
+            if (err && err.code !== 'ENOENT') reject(err)
+            if (stats) {
+                console.log('notes directory already exists')
+                resolve()
+            } else {
+                fs.mkdir('./notes/', (err) => {
+                    if (err) reject(err)
+                    console.log('notes directory created')
+                    resolve()
+                });
+            }
+        });
+    });
+};
+
+/** Create list needed for index view to display */
+const formatList = (list) => {
+    return list.map((item) => {
+            return {
+                id: item.id,
+                title: item.title,
+                text: item.text,
+                created: formatDate(item.timestamp)
+            };
+    });
+}
+
+/** Format the date to create readable timestamp */
+const formatDate = (timestamp) => {
+    const date = new Date(timestamp)
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const hours = (date.getHours() >= 12) ? date.getHours() - 12 : date.getHours();
+    const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+    const ampm = (date.getHours() >= 12) ? "PM" : "AM";
+    return `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()} ${hours}:${minutes} ${ampm}`
+}
+
